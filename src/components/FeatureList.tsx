63cf62d4541ec165d0347701c0260adefdfc513a
Sreenivasa Kamal Nath P <kamalmca@gmail.com>
2025-04-22 08:27:39 +0000

    I want to have a three sections in the features list the fastest section to contain the basic set up related stuff set up technology start selection, develop processes, and whereas the second section, you can just keep it whatever you have you are right now showing don't want to change anything, but the third section to contain more on the addition on top of the overestimation like a buffer at some PMO activities

--- src/components/FeatureList.tsx
+++ src/components/FeatureList.tsx
@@ -1,17 +1,32 @@
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from '@/components/ui/card';
 import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from '@/components/ui/table';
 
+interface Feature {
+  module: string;
+  name: string;
+  multiplier: number;
+  size: string;
+  hours: number;
+}
+
 interface FeatureListProps {
-  features: {
-    module: string;
-    name: string;
-    multiplier: number;
-    size: string;
-    hours: number;
-  }[];
+  features: Feature[];
 }
 
 export default function FeatureList({features}: FeatureListProps) {
+  const basicSetupFeatures = features.filter(feature => 
+    ['Setup Technology Stack Selection', 'Develop Processes'].includes(feature.module)
+  );
+
+  const existingFeatures = features.filter(feature => 
+    !['Setup Technology Stack Selection', 'Develop Processes', 'Buffer', 'PMO Activities'].includes(feature.module) &&
+    !basicSetupFeatures.includes(feature)
+  );
+
+  const additionalActivitiesFeatures = features.filter(feature => 
+    ['Buffer', 'PMO Activities'].includes(feature.module)
+  );
+
   return (
     <Card>
       <CardHeader>
@@ -21,28 +36,92 @@ export default function FeatureList({features}: FeatureListProps) {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Module</TableHead>
-              <TableHead>Feature Name</TableHead>
-              <TableHead>Multiplier</TableHead>
-              <TableHead>Size</TableHead>
-              <TableHead>Hours</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {features.map((feature, index) => (
-              <TableRow key={index}>
-                <TableCell>{feature.module}</TableCell>
-                <TableCell>{feature.name}</TableCell>
-                <TableCell>{feature.multiplier}</TableCell>
-                <TableCell>{feature.size}</TableCell>
-                <TableCell>{feature.hours}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        {/* Basic Setup Section */}
+        {basicSetupFeatures.length > 0 && (
+          <>
+            <h3 className="text-lg font-semibold mb-2">Basic Setup</h3>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Module</TableHead>
+                  <TableHead>Feature Name</TableHead>
+                  <TableHead>Multiplier</TableHead>
+                  <TableHead>Size</TableHead>
+                  <TableHead>Hours</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {basicSetupFeatures.map((feature, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{feature.module}</TableCell>
+                    <TableCell>{feature.name}</TableCell>
+                    <TableCell>{feature.multiplier}</TableCell>
+                    <TableCell>{feature.size}</TableCell>
+                    <TableCell>{feature.hours}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </>
+        )}
+
+        {/* Existing Features Section */}
+        {existingFeatures.length > 0 && (
+          <>
+            <h3 className="text-lg font-semibold mt-4 mb-2">Existing Features</h3>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Module</TableHead>
+                  <TableHead>Feature Name</TableHead>
+                  <TableHead>Multiplier</TableHead>
+                  <TableHead>Size</TableHead>
+                  <TableHead>Hours</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {existingFeatures.map((feature, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{feature.module}</TableCell>
+                    <TableCell>{feature.name}</TableCell>
+                    <TableCell>{feature.multiplier}</TableCell>
+                    <TableCell>{feature.size}</TableCell>
+                    <TableCell>{feature.hours}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </>
+        )}
+
+        {/* Additional Activities Section */}
+        {additionalActivitiesFeatures.length > 0 && (
+          <>
+            <h3 className="text-lg font-semibold mt-4 mb-2">Additional Activities</h3>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Module</TableHead>
+                  <TableHead>Feature Name</TableHead>
+                  <TableHead>Multiplier</TableHead>
+                  <TableHead>Size</TableHead>
+                  <TableHead>Hours</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {additionalActivitiesFeatures.map((feature, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{feature.module}</TableCell>
+                    <TableCell>{feature.name}</TableCell>
+                    <TableCell>{feature.multiplier}</TableCell>
+                    <TableCell>{feature.size}</TableCell>
+                    <TableCell>{feature.hours}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </>
+        )}
       </CardContent>
     </Card>
   );
